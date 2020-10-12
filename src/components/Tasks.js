cd600540d6679fa81299900fdf8cc6ad049bfefb
Xearta <rsmcwow@gmail.com>
2020-10-12 12:16:33 -0400

    Added ability to add tasks based on project and date

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,7 @@ import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
 import { getTitle, getCollatedTitle, collatedTasksExists } from '../helpers';
+import { AddTask } from './AddTask';
 
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
@@ -41,6 +42,8 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
+
+      <AddTask />
     </div>
   );
 };
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
+import { AddTask } from '../AddTask';
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className='header' data-testid='header'>
       <nav>
@@ -10,13 +14,37 @@ export const Header = () => {
         </div>
         <div className='settings'>
           <ul>
-            <li>+</li>
-            <li>
-              <FaPizzaSlice />
+            <li className='settings__add'>
+              <button
+                data-testid='quick-add-task-action'
+                aria-label='Quick add task'
+                type='button'
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}>
+                +
+              </button>
+            </li>
+            <li className='settings__darkmode'>
+              <button
+                data-testid='dark-mode-action'
+                aria-label='Darkmode on/off'
+                type='button'
+                onClick={() => setDarkMode(!darkMode)}>
+                <FaPizzaSlice />
+              </button>
             </li>
           </ul>
         </div>
       </nav>
+
+      <AddTask
+        showAddTaskMain={false}
+        shouldShowMain={shouldShowMain}
+        showQuickAddTask={showQuickAddTask}
+        setShowQuickAddTask={setShowQuickAddTask}
+      />
     </header>
   );
 };
